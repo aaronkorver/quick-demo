349fe0ecefe36444b78f33b04bd45c9f5a121db0
Aaron Korver <aaron.korver@gmail.com>
2022-09-21 16:40:30 -0500

    Add files via upload

---
+++ src/main/java/com/example/walgreensdemo/WalgreensDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.walgreensdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalgreensDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalgreensDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/walgreensdemo/WalgreensDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walgreensdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalgreensDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
